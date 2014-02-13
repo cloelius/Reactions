e1cb850af35a857156c3dfbbe5680f5e49ad172c
cloelius <cloelius1@gmail.com>
2014-02-13 11:19:01 -0500

    Trying to make doing multiple plots

--- Project 2/makefrescoplots.py
+++ Project 2/makefrescoplots.py
@@ -11,13 +11,23 @@ def GetSMatData(f):
     mods=np.abs(arr[:,0]+1j*arr[:,1])
     Ls=arr[:,2]
     return np.array([Ls,mods])
-def twodplot(x,y,title,xaxis,yaxis):
-    fig=plt.figure()
-    ax2=fig.add_subplot(111)
-    ax2.plot(x,y)
-    plt.title(title)
-    plt.xlabel(xaxis)
-    plt.ylabel(yaxis)
+def twodplot(x,y,title,xaxis,yaxis,fig=None,leg=None):
+    if(fig==None):
+        fig=plt.figure()
+        ax2=fig.add_subplot(111)
+        plt.title(title)
+        plt.xlabel(xaxis)
+        plt.ylabel(yaxis)
+    else :
+        ax2=fig.gca()
+    if leg != None:
+            ax2.plot(x,y,label=leg)
+    else:
+            ax2.plot(x,y)
+    handles, labels = ax2.get_legend_handles_labels()
+    if any(labels):
+        ax2.legend(handles[::-1], labels[::-1])
+    return fig
 di='C:\\Users\\Charles\\Documents\\frescoreactions\\reactions\\'
 Neu50=GetCrossSectionData(di+"Ni58Neutron50MeV\\fort.16")
 Neu5=GetCrossSectionData(di+"Ni58Neutron5MeV\\fort.16")
@@ -27,9 +37,9 @@ NeuWeakcs=GetCrossSectionData(di+"NeutronWeakImaginary5MeV\\fort.16")
 NeuStrongcs=GetCrossSectionData(di+"NeutronStrongImaginary5MeV\\fort.16")
 NeuWeaks=GetSMatData(di+"NeutronWeakImaginary5MeV\\fort.7")
 NeuStrongs=GetSMatData(di+"NeutronStrongImaginary5MeV\\fort.7")
-twodplot(Neu5[:,0],Neu5[:,1],"Neutron-Nickel 58 Cross Section for 5 MeV","Angle (Degrees)", "Differential Cross Section(relative to Rutherford")
-twodplot(Neu50[:,0],Neu50[:,1],"Neutron-Nickel 58 Cross Section for 50 MeV","Angle (Degrees)", "Differential Cross Section(relative to Rutherford")
-twodplot(Neudiff[:,0],Neudiff[:,1],"Neutron-Nickel 58 Cross Section for 5 MeV with increased diffusiveness","Angle (Degrees)", "Differential Cross Section(relative to Rutherford")
+Neuen=twodplot(Neu5[:,0],Neu5[:,1],"Neutron-Nickel 58 Cross Section","Angle (Degrees)", "Differential Cross Section(relative to Rutherford",leg="5 MeV")
+twodplot(Neu50[:,0],Neu50[:,1],"Neutron-Nickel 58 Cross Section for 50 MeV","Angle (Degrees)", "Differential Cross Section(relative to Rutherford",fig=Neuen,leg="50 MeV")
+twodplot(Neudiff[:,0],Neudiff[:,1],"Neutron-Nickel 58 Cross Section for 5 MeV with increased diffusiveness","Angle (Degrees)", "Differential Cross Section(relative to Rutherford",leg="hi")
 twodplot(Neurad[:,0],Neurad[:,1],"Neutron-Nickel 58 Cross Section for 5 MeV with increased radius","Angle (Degrees)", "Differential Cross Section(relative to Rutherford")
 twodplot(NeuStrongcs[:,0],NeuStrongcs[:,1],"Neutron-Nickel 58 Cross Section for 5 MeV with strong imaginary potential","Angle (Degrees)", "Differential Cross Section(relative to Rutherford")
 twodplot(NeuWeakcs[:,0],NeuWeakcs[:,1],"Neutron-Nickel 58 Cross Section for 5 MeV with weak imaginary potential","Angle (Degrees)", "Differential Cross Section(relative to Rutherford")
